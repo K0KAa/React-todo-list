08a3a6f9f03bf405ad2942bd7c985e58c0160d5a
Kushal Kattel <kushalkattel0@gmail.com>
2021-05-08 10:25:51 +0545

    app component structure

--- src/App.js
+++ src/App.js
@@ -13,15 +13,48 @@ class App extends Component {
       { id: 2, title: "make breakfast" },
     ],
     id: uuid(),
+    item: "",
+    editItem: false,
+  };
+
+  handleChange = (e) => {
+    console.log("handle Change");
+  };
+
+  handleSubmit = (e) => {
+    console.log("handle Submit");
+  };
+
+  clearList = () => {
+    console.log("clear list ");
+  };
+
+  handleDelete = (id) => {
+    console.log(`handle delete ${id} `);
+  };
+
+  handleEdit = (id) => {
+    console.log(`handle edit ${id}`);
   };
 
   render() {
-    console.log(this.state);
     return (
-      <div className="App">
+      <div className="container">
         <div className="row">
-          <TodoInput />
-          <TodoList />
+          <div className="col--10 mx-autto col-md-8 mt-5">
+            <h3 className="text-capitalize texxt-center">todo Input</h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              hanndleSubmit={this.handleSubmit}
+            />
+            <TodoList
+              items={this.state.items}
+              clearList={this.clearList}
+              handleDelete={this.handleDelete}
+              handleEdit={this.handleEdit}
+            />
+          </div>
         </div>
       </div>
     );
