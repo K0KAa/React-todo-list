564a3f7f42df11cc879a155fb9b0c3088bd00e89
Kushal Kattel <kushalkattel0@gmail.com>
2021-05-08 09:53:05 +0545

    pictures added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
-import uuid from "uuid";
+import { v4 as uuid } from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
 class App extends Component {
+  state = {
+    items: [
+      { id: 1, title: "wake up" },
+      { id: 2, title: "make breakfast" },
+    ],
+    id: uuid(),
+  };
+
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <div className="row">
